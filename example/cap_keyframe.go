636aebf5ed61b51bad87cd06ef984001aad41025
ChaseWang <chase.w@qq.com>
2018-05-08 09:48:47 +0800

    update example

--- example/cap_keyframe.go
+++ example/cap_keyframe.go
@@ -59,58 +59,5 @@ func main() {
 		}
 		packet.Free()
 	}
-	/*
-	C.av_register_all()
-	var pFormatCtx *C.struct_AVFormatContext
-	fi := "sample.mp4"
-	if int(C.avformat_open_input(&pFormatCtx, C.CString(fi), nil, nil)) < 0 {
-		panic("can not find stream info")
-	}
-	if int(C.avformat_find_stream_info(pFormatCtx, nil)) < 0 {
-		panic("can not find video stream")
-	}
-	//C.av_dump_format(pFormatCtx, 0, C.CString(""), 0)
-	var pCodecCtx *C.struct_AVCodecContext
-	var streams *C.struct_AVStream
-	videoStream := -1
-	for i := 0; i < int(pFormatCtx.nb_streams); i++ {
-		offset := (unsafe.Sizeof(unsafe.Pointer(*pFormatCtx.streams)) * uintptr(i))
-		streams = *(**C.struct_AVStream)(unsafe.Pointer(uintptr(unsafe.Pointer(pFormatCtx.streams)) + offset))
-		pCodecCtx = (*C.struct_AVCodecContext)(streams.codec)
-		if pCodecCtx.codec_type == C.AVMEDIA_TYPE_VIDEO {
-			videoStream = i
-			break
-		}
-	}
-	if videoStream == -1 {
-		panic("can not find video stream")
-	}
-	var pCodec *C.struct_AVCodec = C.avcodec_find_decoder(pCodecCtx.codec_id)
-	if pCodec == nil {
-		panic("can not find codec")
-	}
-	if int(C.avcodec_open2(pCodecCtx, pCodec, nil)) < 0 {
-		panic("can not open codec")
-	}
-	pFrame := C.av_frame_alloc()
-	i := 0
-	var packet C.struct_AVPacket
-	for int(C.av_read_frame(pFormatCtx, &packet)) >= 0 {
-		if int(packet.stream_index) == videoStream {
-			var frameFinished C.int
-			C.avcodec_decode_video2(pCodecCtx, pFrame, &frameFinished, &packet)
-			if frameFinished > 0 {
-				if pFrame.key_frame == 1 {
-					img := getImage(pFrame)
-					go saveFrame(img, 60, i)
-					i++
-				}
-			}
-		}
-		C.av_free_packet(&packet)
-	}
-	C.av_frame_free(&pFrame)
-	C.avcodec_close(pCodecCtx)
-	C.avformat_close_input(&pFormatCtx)*/
 	fmt.Printf("Timecosts：%ds\n", time.Now().Unix()-start)
 }
